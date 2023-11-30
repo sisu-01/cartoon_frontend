d01048a2db6df9127a74adbadfcd8d740c910aa2
sisu-01 <tesv010615@naver.com>
2023-11-30 17:24:57 +0900

    info api 연동

--- src/routes/Info.js
+++ src/routes/Info.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 function Info() {
+    const [cartoonList, setCartoonList] = useState();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const id = searchParams.get('id');
+    const nickname = searchParams.get('nickname');
+
+    async function getInfo() {
+        await fetch(`http://localhost:4000/info?page=0&id=${id}&nickname=${nickname}`)
+        .then(response => response.json())
+        .then(data => {
+            setCartoonList(data.result);
+        })
+    }
+
+    useEffect(()=> {
+        getInfo();
+    }, []);
+
+    function getLoop() {
+        const newArr = [];
+        for(const key in cartoonList) {
+            const i = cartoonList[key];
+            newArr.push(
+                <tr key={key}>
+                    <td>{i['title']}</td>
+                    <td>{i['date']}</td>
+                    <td>{i['recommend']}</td>
+                </tr>
+            )
+        }
+        return newArr;
+    }
+
     return (
         <div className='Info'>
-            Info
+            Info<br/>
+            {id=='a'?'유동':`아이디: ${id}`}<br/>
+            닉네임: {nickname}
+            <hr/>
+            <div>
+                <table>
+                <thead>
+                    <tr>
+                        <th>title</th>
+                        <th>date</th>
+                        <th>rec</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {getLoop()}
+                </tbody>
+                </table>
+            </div>
         </div>
     );
 }
