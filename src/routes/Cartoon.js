3ac70e004526502c41322e0ce7c61a061402672a
sisu-01 <tesv010615@naver.com>
2023-11-30 15:13:13 +0900

    기초 리스트

--- src/routes/Cartoon.js
+++ src/routes/Cartoon.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 function Cartoon() {
+    const [cartoonList, setCartoonList] = useState();
+
+    async function getCartoon() {
+        await fetch('http://localhost:4000/cartoon/0')
+        .then(response => response.json())
+        .then(data => {
+            setCartoonList(data.result);
+        })
+    }
+
+    useEffect(() => {
+        getCartoon();
+    }, []);
+
+    function getLoop() {
+        const newArr = [];
+        for(const key in cartoonList) {
+            const i = cartoonList[key];
+            newArr.push(
+                <tr key={key}>
+                    <td>{i['id']}</td>
+                    <td>{i['title']}</td>
+                    <td>{i['writer_nickname']}</td>
+                    <td>{i['date']}</td>
+                    <td>{i['recommend']}</td>
+                </tr>
+            )
+        }
+        return newArr;
+    }
+
     return (
         <div className='Cartoon'>
-            Cartoon
+            <table>
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>title</th>
+                        <th>writer</th>
+                        <th>date</th>
+                        <th>rec</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {getLoop()}
+                </tbody>
+            </table>
         </div>
     );
 }
