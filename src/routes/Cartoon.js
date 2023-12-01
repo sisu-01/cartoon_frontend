91ea796317e5816ec1df579f0438e80342dd5bf5
sisu-01 <tesv010615@naver.com>
2023-12-01 11:48:56 +0900

    id 빼

--- src/routes/Cartoon.js
+++ src/routes/Cartoon.js
@@ -31,8 +31,7 @@ function Cartoon() {
                 const date = common.dateFormat(i['date']);
                 newArr.push(
                     <tr key={key}>
-                        <td><a href={`https://gall.dcinside.com/board/view/?id=cartoon&no=${i['id']}`} target='blank'>{i['id']}</a></td>
-                        <td>{i['title']}</td>
+                        <td><a href={`https://gall.dcinside.com/board/view/?id=cartoon&no=${i['id']}`} target='blank'>{i['title']}</a></td>
                         <td><Link to={`/info?id=${i['writer_id']}&nickname=${i['writer_nickname']}`}>{i['writer_nickname']}</Link></td>
                         <td>{date}</td>
                         <td>{i['recommend']}</td>
@@ -60,7 +59,6 @@ function Cartoon() {
             <table>
                 <thead>
                     <tr>
-                        <th>id</th>
                         <th>title</th>
                         <th>writer</th>
                         <th>date</th>
