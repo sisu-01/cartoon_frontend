b895b521d5fe75588512db907849bd2cef6e6050
sisu-01 <tesv010615@naver.com>
2024-02-01 23:22:24 +0900

    컨테이너 추가

--- src/index.js
+++ src/index.js
@@ -6,11 +6,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './layouts/Header';
 import Footer from './layouts/Footer';
 
+import Container from 'react-bootstrap/Container';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const containerStyle = {
+    borderColor: 'blue',
+    borderWidth: '1px',
+    borderStyle: 'solid',
+  };
 root.render(
     <BrowserRouter>
         <Header />
-        <App />
+        <Container fluid='md' style={containerStyle}>
+            <App />
+        </Container>
         <Footer />
     </BrowserRouter>
 );
