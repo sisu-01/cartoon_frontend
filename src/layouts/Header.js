a46e432fa155f0a5cbf238e9ab0ebeb20443a1dc
sisu-01 <tesv010615@naver.com>
2024-02-04 22:23:54 +0900

    헤더 이쁘게

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+  
 function Header() {
     return (
         <header>
-            <Link to="/">home</Link>&nbsp;&nbsp;|&nbsp;&nbsp;
-            <Link to="/cartoon">cartoon</Link>&nbsp;&nbsp;|&nbsp;&nbsp;
-            <Link to="/writer">writer</Link>&nbsp;&nbsp;|&nbsp;&nbsp;
-            <Link to="/series">series ( 시험적 기능 )</Link>
-            <hr/>
+            <Navbar className="bg-body-tertiary">
+                <Container fluid='md'>
+                    <Navbar.Brand as={Link} to="/">이름-미정</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/">Root</Nav.Link>
+                        <Nav.Link as={Link} to="/cartoon">Cartoon</Nav.Link>
+                        <Nav.Link as={Link} to="/writer">Writer</Nav.Link>
+                        <Nav.Link as={Link} to="/series">Test</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
         </header>
     );
 };
