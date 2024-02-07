e23a04c63ae8e708b85c9eec2cc19cbc3554f55d
sisu-01 <tesv010615@naver.com>
2024-02-07 18:29:57 +0900

    최소 개추 select

--- src/components/Cut.js
+++ src/components/Cut.js
@@ -1,3 +1,5 @@
+import Form from 'react-bootstrap/Form';
+
 /**
  * 개추 컷 이상 출력~
  * @param {number} value select 선택 값
@@ -5,17 +7,22 @@
  */
 function Cut(props) {
     return (
-        <div>
-            <select id='cut' onChange={({target: {value}}) => props.handler(value)} value={props.value}>
-                <option value>추컷</option>
+        <>
+            <Form.Select
+                id='recommend_cut'
+                size='sm'
+                onChange={({target: {value}}) => props.handler(value)}
+                value={props.value}
+            >
+                <option value>0</option>
                 <option value={50}>50</option>
                 <option value={100}>100</option>
                 <option value={250}>250</option>
                 <option value={500}>500</option>
                 <option value={1000}>1000</option>
-            </select>
-            <label htmlFor='cut'>개추컷</label>
-        </div>
+            </Form.Select>
+            <label htmlFor='recommend_cut'>최초 개수 몇 개 이상 따흐흑</label>
+        </>
     );
 }
 
