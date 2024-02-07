3279f0d3b5c3a1708ec215a955ff0a2987110004
sisu-01 <tesv010615@naver.com>
2024-02-07 18:23:20 +0900

    개추순 checkbox

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,3 +1,5 @@
+import Form from 'react-bootstrap/Form';
+
 /**
  * 개추 순으로 정렬 체크박스다~
  * @param {boolean} checked 체크박스 선택 유무
@@ -5,10 +7,13 @@
  */
 function Sort(props) {
     return (
-        <div>
-            <input type='checkbox' id='sort' checked={props.checked} onChange={({ target: { checked } }) => props.handler(checked)} />
-            <label htmlFor='sort'>개추순으로 정렬</label>
-        </div>
+        <Form.Check
+            type='switch'
+            id={`sortt`}
+            checked={props.checked}
+            onChange={({ target: { checked } }) => props.handler(checked)}
+            label={`개추순으로 정렬`}
+        />
     );
 }
 
