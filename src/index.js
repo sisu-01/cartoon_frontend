192f58f24ea8a25f1973071842e68603a5e680eb
sisu-01 <tesv010615@naver.com>
2024-02-08 15:23:51 +0900

    stylel.css 생성

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import {BrowserRouter} from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+
+import App from './App';
 import Header from './layouts/Header';
 import Footer from './layouts/Footer';
 
-import Container from 'react-bootstrap/Container';
+import './style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const containerStyle = {
