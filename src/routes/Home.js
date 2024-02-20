7e543b2dbe960db58aeb9a743ac62670c1940ace
sisu-01 <tesv010615@naver.com>
2024-02-20 14:10:40 +0900

    김풍키즈 설명

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Home() {
     return (
-        <div>
-            Home
+        <div className='poong'>
+            <img src='/poong.jpg' />
         </div>
     );
 }
