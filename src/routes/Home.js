8d3dfd691547b80d2b57d1e5c162acc6d67b429c
sisu-01 <tesv010615@naver.com>
2024-02-27 14:13:39 +0900

    Home.js alt

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,7 +7,7 @@ function Home() {
     return (
         <div className='poong'>
             <MetaTag title='이름 미정' description='카연갤 필터 사이트얌' url='/' />
-            <img src='/poong.jpg' />
+            <img src='/poong.jpg' alt='카툰-연재 갤러리는 김풍이 만든 사이트입니다.'/>
         </div>
     );
 }
