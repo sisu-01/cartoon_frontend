8cac75fa1bd1184c4df4d94de7ed5074277f2350
sisu-01 <tesv010615@naver.com>
2024-03-12 13:59:00 +0900

    빌드

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     return (
         <footer className='footer'>
             <Container fluid='md'>
-                <span className='text-white'>푸터 입니다.</span>
+                <span className='text-white'>푸터 입니다. build 24.03.12</span>
             </Container>
         </footer>
     );
