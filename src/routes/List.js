483ef5d63b56ac1eeff8c1f17c77c67a17bf4154
sisu-01 <tesv010615@naver.com>
2024-03-20 18:12:19 +0900

    useNavigate 도입

--- src/routes/List.js
+++ src/routes/List.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
@@ -65,49 +65,81 @@ function List() {
 
     //만화 목록이랑 페이징 들어갈 컴포넌트
     function List() {
+        const navigate = useNavigate();
 
         //url 파라미터들
         const searchParams = new URLSearchParams(window.location.search);
-        const tempPage = useRef(Number(searchParams.get('page')) || 1);
+        const [pageState, setPageState] = useState(Number(searchParams.get('page')) || 1);
+        const pageRef = useRef(pageState);
 
         //페이징에 필요한 정보들
         const [cartoonList, setCartoonList] = useState();
         const [perPage, setPerPage] = useState();
         const [count, setCount] = useState();
 
+        //수정 메타데이터를 위한 것?
         const location = window.location;
         const currentUrl = location.pathname+location.search;
 
         //최초 실행
         useEffect(() => { 
-            getList();
+            getList(true);
+
+            function handleBack() {
+                const popParams = new URLSearchParams(window.location.search);
+                pageRef.current = Number(popParams.get('page')) || 1;
+                getList(true);
+            }
+            // 뒤로가기 이벤트를 감지할 때 handleBack 함수를 실행
+            window.addEventListener('popstate', handleBack);
+            return () => {
+                // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
+                window.removeEventListener('popstate', handleBack);
+            };
         }, []);
 
         //네비게이터에 쓸 url 제조기
         function getUrl(p=1) {
-            tempPage.current = Number(p);
+            pageRef.current = Number(p);
             let url = '';
-            url += `/list?id=${id}&page=${tempPage.current}`
+            url += `/list?id=${id}&page=${pageRef.current}`
             return url;
         }
+        /**
+         * 네비게이터에 쓸 url 제조기 object 담아서 보내시오..
+         * @param {number} page 페이지
+         * @returns {string} url
+         */
+        function getUrl({
+            page=false
+        }) {
+           //false라면? 즉 값을 받지 않는다면? 기본 state 적용
+           if (page===false) page = pageState
+   
+           let url = '';
+           url += `/list?id=${id}&page=${Number(page)}`
 
-        //브라우저 뒤로가기, 앞으로가기 감지
-        window.onpopstate = () => {
-            common.popNavigate({
-                page: tempPage,
-                callback: getList,
-            });
-        };
+           return url;
+        }
 
-        //목록 가져오는 api
-        function getList() {
+        /**
+         * 목록 가져오는 api
+         * @param {boolean} isFirstOrPop useEffect 처음이냐 아니냐
+         */
+        function getList(isFirstOrPop) {
             let url = '';
             url += API_SERVER;
-            url += `/list?id=${id}&page=${tempPage.current}`;
+            url += `/list?id=${id}&page=${pageRef.current}`;
             fetch(url)
             .then(response => response.json())
             .then(data => {
                 if(data['ok']){
+                    if(!isFirstOrPop) {
+                        navigate(getUrl({
+                            page: pageRef.current
+                        }));
+                    }
+                    setPageState(pageRef.current);
                     setCartoonList(data['list']);
                     setPerPage(data['perPage']);
                     setCount(data['count']);
@@ -158,9 +190,21 @@ function List() {
             }
         }
 
+        /**
+         * ref 값 갱신하고 목록 api 호출
+         * @param {number} page 페이지
+         */
+        function setRefAndFetch({
+            page=false,
+        }) {
+            //false가 아니라면? 즉 값을 받았다면?
+            if (page!==false) pageRef.current = page;
+            getList();
+        }
+
         //페이징 버튼 핸들러
         function pageHandler(page) {
-            common.navigate(getUrl(page), getList);
+            setRefAndFetch({page: page});
         };
 
         return (
@@ -168,7 +212,7 @@ function List() {
                 <MetaTag title={init['title']} description={`${init['writer_nickname']}의 연재 만화`} url={currentUrl} />
                 {renderCartoonList()}
                 <div className='mt-2'>
-                    <Paging page={tempPage.current} perPage={perPage} count={count} pageBtn={5} handler={pageHandler}/>
+                    <Paging page={pageRef.current} perPage={perPage} count={count} pageBtn={5} handler={pageHandler}/>
                 </div>
             </div>
         );
